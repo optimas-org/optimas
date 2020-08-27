a5e1c7cb8071b6b03a692ff1626a78ba9803b5e6
Remi Lehe <remi.lehe@normalesup.org>
2020-08-26 20:03:56 -0400

    Extend parameters ; add requirement

--- optimization_folder/sim_specific/varying_parameters.py
+++ optimization_folder/sim_specific/varying_parameters.py
@@ -3,6 +3,6 @@ from collections import OrderedDict
 # List the names of the varying parameters, and their range of value
 # The names must be the same as those used in 'template_fbpic_script.py'
 varying_parameters = OrderedDict({
-    'adjust_factor1': [0.98, 1.02],
-    'adjust_factor2': [0.98, 1.02],
+    'adjust_factor1': [0.7, 1.],
+    'adjust_factor2': [0.7, 1.],
 })
