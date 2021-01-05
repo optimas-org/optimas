fbdf3bfb830d8894bd17a4c03df46b46faee9e39
Angel Ferran Pousa <angel.ferran.pousa@desy.de>
2021-01-05 13:57:37 +0100

    Add method to determine fidelity type and lenght

--- source/run_libensemble.py
+++ source/run_libensemble.py
@@ -56,6 +56,26 @@ from sim_specific.analysis_script import analyzed_quantities
 if is_mf:
     from sim_specific.mf_parameters import mf_parameters
 
+
+def determine_fidelity_type_and_length(mf_parameters):
+    """
+    Determine the type of the fidelity (i.e. float, int, str...) and, if it
+    is a string, also its length.
+    """
+    # Check that all fidelities in 'range' are of the same type.
+    fidel_types = [type(z) for z in mf_parameters['range']]
+    if fidel_types.count(fidel_types[0]) != len(fidel_types):
+        raise ValueError("The fidelities in 'range' are of different types.")
+    fidel_type = fidel_types[0]
+    fidel_len = None
+    # If fidelities are strings, determine the lenght of the longest one
+    # so that it can be fully stored in a numpy array.
+    if fidel_type == str:
+        str_lengths = [len(z) for z in mf_parameters['range']]
+        fidel_len = max(str_lengths)
+    return fidel_type, fidel_len
+
+
 libE_logger.set_level('INFO')
 nworkers, is_master, libE_specs, _ = parse_args()
 
