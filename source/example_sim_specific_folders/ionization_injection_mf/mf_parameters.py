12bd02c61ab8b8354dab36927e489282a452b245
Angel Ferran Pousa <angel.ferran.pousa@desy.de>
2021-01-26 19:32:47 +0100

    Remove old examples

--- source/example_sim_specific_folders/ionization_injection_mf/mf_parameters.py
+++
@@ -1,9 +0,0 @@
-""""
-Contains the parameters for multi-fidelity optimization.
-"""
-mf_parameters = {
-    'name': 'resolution',
-    'range': [2., 4.],
-    'discrete': False,
-    'cost_func': lambda z: z[0]**2
-}
