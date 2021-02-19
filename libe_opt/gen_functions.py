725c1470931780b2e45d26edd72ddb11ea57606d
Remi Lehe <remi.lehe@normalesup.org>
2021-02-19 10:24:15 -0500

    Modify import statement for gen_funcs

--- libe_opt/gen_functions.py
+++ libe_opt/gen_functions.py
@@ -1,24 +1,23 @@
-from libensemble.gen_funcs.persistent_uniform_sampling import (
-    persistent_uniform)
-from libensemble.gen_funcs.persistent_gp import (
-    persistent_gp_gen_f, persistent_gp_mf_gen_f, persistent_gp_mf_disc_gen_f)
-import libensemble.gen_funcs as libe_genf
-libe_genf.rc.aposmm_optimizers = 'nlopt'
-from libensemble.gen_funcs.persistent_aposmm import aposmm
-
-
-gen_functions = {
-    'random': persistent_uniform,
-    'bo': persistent_gp_gen_f,
-    'bo_mf': persistent_gp_mf_gen_f,
-    'bo_mf_disc': persistent_gp_mf_disc_gen_f,
-    'aposmm': aposmm
-}
-
 
 def get_generator_function(gen_type):
-    if gen_type in gen_functions:
-        return gen_functions[gen_type]
+    if gen_type == 'aposmm':
+        import libensemble.gen_funcs
+        libensemble.gen_funcs.rc.aposmm_optimizers = 'nlopt'
+        from libensemble.gen_funcs.persistent_aposmm import aposmm
+        return aposmm
+    elif gen_type == 'random':
+        from libensemble.gen_funcs.persistent_uniform_sampling import \
+            persistent_uniform
+        return persistent_uniform
+    elif gen_type in 'bo':
+        from libensemble.gen_funcs.persistent_gp import persistent_gp_gen_f
+        return persistent_gp_gen_f
+    elif gen_type in 'bo_mf':
+        from libensemble.gen_funcs.persistent_gp import persistent_gp_mf_gen_f
+        return persistent_gp_mf_gen_f
+    elif gen_type in 'bo_mf_disc':
+        from libensemble.gen_funcs.persistent_gp import persistent_gp_mf_disc_gen_f
+        return persistent_gp_mf_disc_gen_f
     else:
         raise ValueError(
             "Generator type '{}' not recognized.".format(gen_type))
