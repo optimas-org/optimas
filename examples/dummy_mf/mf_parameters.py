6700607ab03e68f2256cd0088d63ce48e271e3e2
Remi Lehe <remi.lehe@normalesup.org>
2021-04-06 10:42:19 -0700

    Add automated test for multi-fidelity cases

--- examples/dummy_mf/mf_parameters.py
+++ examples/dummy_mf/mf_parameters.py
@@ -6,5 +6,5 @@ mf_parameters = {
     'name': 'resolution',
     'range': [1, 8],
     'discrete': False,
-    'cost_func': lambda z: z[0]**2
+    'cost_func': lambda z: z[0]
 }
--- examples/dummy_mf_disc/mf_parameters.py
+++ examples/dummy_mf_disc/mf_parameters.py
@@ -4,7 +4,7 @@ Contains the parameters for multi-fidelity optimization.
 
 mf_parameters = {
     'name': 'resolution',
-    'range': [1, 2, 4, 8],
+    'range': [1, 2, 4],
     'discrete': True,
-    'cost_func': lambda z: int(z[0][0])**2
+    'cost_func': lambda z: z[0][0]
 }
