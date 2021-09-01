972c222edded927593473e8d92218af4f5d1b045
Angel Ferran Pousa <angel.ferran.pousa@desy.de>
2021-09-01 16:31:44 +0200

    Update example

--- examples/dummy_mt/run_example.py
+++ examples/dummy_mt/run_example.py
@@ -3,9 +3,10 @@ from libe_opt.ensemble_runner import run_ensemble
 
 from varying_parameters import varying_parameters
 from analysis_script import analyze_simulation, analyzed_quantities
+from mt_parameters import mt_parameters
 
 
-gen_type = 'bo_mt'
+gen_type = 'bo'
 sim_max = 10
 run_async = True
 nworkers, is_master, libE_specs, _ = parse_args()
@@ -13,5 +14,5 @@ nworkers, is_master, libE_specs, _ = parse_args()
 run_ensemble(
     nworkers, sim_max, is_master, gen_type,
     analyzed_params=analyzed_quantities, var_params=varying_parameters,
-    analysis_func=analyze_simulation,
+    analysis_func=analyze_simulation, mt_params=mt_parameters,
     libE_specs=libE_specs, run_async=run_async)
