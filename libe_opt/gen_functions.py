ce05c4ff7aa8a5af263f39c02380db24d4ef29a2
Remi Lehe <remi.lehe@normalesup.org>
2021-12-02 18:16:51 -0800

    Make the code more robust when ax or dragonfly are not installed

--- libe_opt/gen_functions.py
+++ libe_opt/gen_functions.py
@@ -6,10 +6,6 @@ from .persistent_gp import (
 
 gen_functions = {
     'random': persistent_uniform,
-    'bo': persistent_gp_gen_f,
-    'bo_mf': persistent_gp_mf_gen_f,
-    'bo_mf_disc': persistent_gp_mf_disc_gen_f,
-    'bo_mt': persistent_gp_mt_ax_gen_f
 }
 
 # Try to import `aposmm` if avaiable (heavier dependencies)
@@ -21,17 +17,55 @@ try:
 except ImportError:
     pass
 
+# Try to import Dragonfly's Bayesian optimizers
+try:
+    from .dragonfly_gen_funcs import \
+       persistent_gp_gen_f, persistent_gp_mf_gen_f, persistent_gp_mf_disc_gen_f
+    gen_functions['bo'] = persistent_gp_gen_f
+    gen_functions['bo_mf'] = persistent_gp_mf_gen_f
+    gen_functions['bo_mf_disc'] = persistent_gp_mf_disc_gen_f,
+except ImportError:
+    pass
+
+# Try to import Ax's Multi-task optimizer
+try:
+    from .ax_gen_funcs import persistent_gp_mt_ax_gen_f
+    gen_functions['bo_mt'] = persistent_gp_mt_ax_gen_f
+except ImportError:
+    pass
+
 def get_generator_function(gen_type):
     if gen_type in gen_functions:
         return gen_functions[gen_type]
     elif gen_type == 'aposmm':
         raise RuntimeError(
-        """You are trying to use APOSMM, but that it could not be imported. \n
+        """You are trying to use APOSMM, but it could not be imported. \n
        Please make sure that the following lines work in your Python environment:
 
        import libensemble.gen_funcs as libe_genf
        libe_genf.rc.aposmm_optimizers = 'nlopt'
        from libensemble.gen_funcs.persistent_aposmm import aposmm
+       """)
+   elif gen_type in ['bo', 'bo_mf', 'bo_mf_disc']:
+        raise RuntimeError(
+        """You are trying to use dragonfly's Bayesian optimization, but it
+        could not be imported. \n  Please make sure that the following lines
+        work in your Python environment:
+
+       import dragonfly
+
+       If not, this can be installed with `pip install dragonfly`
+       """)
+   elif gen_type == 'bo_mt':
+        raise RuntimeError(
+        """You are trying to use Ax's multi-task optimization, but it
+        could not be imported. \n Please make sure that the following lines
+        work in your Python environment:
+
+       import ax
+       import pandas
+
+       If not, this can be installed with `pip install ax-platform pandas`
        """)
     else:
         raise ValueError(
