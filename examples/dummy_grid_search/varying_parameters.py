19555ae535d105d4aaadfd02e2f66511a85e4f1b
Angel Ferran Pousa <angel.ferran.pousa@desy.de>
2022-11-19 17:43:50 +0100

    Add grid search example

---
+++ examples/dummy_grid_search/varying_parameters.py
@@ -0,0 +1,8 @@
+from collections import OrderedDict
+
+# List the names of the varying parameters, and their range of value
+# The names must be the same as those used in 'template_fbpic_script.py'
+varying_parameters = OrderedDict({
+    'x0': [0, 15, 5],
+    'x1': [0, 15, 7],
+})
