30a2f1e11c3720ade47115ef3e78b2e6aa6d3f62
Angel Ferran Pousa <angel.ferran.pousa@desy.de>
2022-11-21 00:10:25 +0100

    Remove custom `get_MTGP`

--- libe_opt/search_methods/bayesian_optimization/ax_developer/gen_functions.py
+++ libe_opt/search_methods/bayesian_optimization/ax_developer/gen_functions.py
@@ -1,26 +1,22 @@
 import os
+from copy import deepcopy
 
 import numpy as np
 import torch
-from libensemble.message_numbers import STOP_TAG, PERSIS_STOP, FINISHED_PERSISTENT_GEN_TAG, EVAL_GEN_TAG
-from libensemble.tools.persistent_support import PersistentSupport
-from libensemble.resources.resources import Resources
-
-
-from copy import deepcopy
-
 from ax.runners import SyntheticRunner
 from ax.storage.json_store.save import save_experiment
 from ax.storage.metric_registry import register_metric
 from ax.storage.runner_registry import register_runner
-from ax.core.search_space import SearchSpace
-from ax.modelbridge.factory import get_sobol
+from ax.modelbridge.factory import get_sobol, get_MTGP
 from ax.core.observation import ObservationFeatures
-from ax.core.multi_type_experiment import MultiTypeExperiment
 from ax.core.generator_run import GeneratorRun
+from libensemble.message_numbers import (
+    STOP_TAG, PERSIS_STOP, FINISHED_PERSISTENT_GEN_TAG)
+from libensemble.resources.resources import Resources
 
 from .ax_metric import AxMetric
 from .ax_runner import AxRunner
+
 
 def persistent_gp_mt_ax_gen_f(H, persis_info, gen_specs, libE_info):
     """
@@ -47,7 +43,7 @@ def persistent_gp_mt_ax_gen_f(H, persis_info, gen_specs, libE_info):
     n_init_hifi = gen_specs['user']['mt_params']['n_init_hifi']
     n_init_lofi = gen_specs['user']['mt_params']['n_init_lofi']
     n_opt_hifi = gen_specs['user']['mt_params']['n_opt_hifi']
-    n_opt_lofi = gen_specs['user']['mt_params']['n_opt_lofi']    
+    n_opt_lofi = gen_specs['user']['mt_params']['n_opt_lofi']
 
     # Create runner.
     ax_runner = AxRunner(libE_info, gen_specs)
@@ -60,7 +56,8 @@ def persistent_gp_mt_ax_gen_f(H, persis_info, gen_specs, libE_info):
     # TODO: Implement reading past history (by reading saved experiment or
     # libEnsemble hystory file).
 
-    # Batch limit for the initialization of the aquisition function optimization.
+    # Batch limit for the initialization of the optimization of the
+    # aquisition function.
     init_batch_limit = 1000
 
     # Receive information from the manager (or a STOP_TAG)
@@ -71,7 +68,8 @@ def persistent_gp_mt_ax_gen_f(H, persis_info, gen_specs, libE_info):
 
         if model_iteration == 0:
             # Initialize with sobol sample.
-            for model, n_gen in zip([hifi_task, lofi_task], [n_init_hifi, n_init_lofi]):
+            for model, n_gen in zip([hifi_task, lofi_task],
+                                    [n_init_hifi, n_init_lofi]):
                 s = get_sobol(exp.search_space, scramble=True)
                 gr = s.gen(n_gen)
                 trial = exp.new_batch_trial(trial_type=model, generator_run=gr)
@@ -97,12 +95,13 @@ def persistent_gp_mt_ax_gen_f(H, persis_info, gen_specs, libE_info):
 
             # 2) Find the best points for the high fidelity task.
 
-            # Too large initialization batches can lead to out-of-memory errors.
+            # Too large initialization batches can lead to out-of-memory
+            # errors.
             # The loop below tries to generate the next points to evaluate
             # using `init_batch_limit`. If an RuntimeError is raised during
             # generation (namely an out-of-memory error), `init_batch_limit`
             # is divided by two and a new attempt is made. This is repeated
-            # until the generation runs successfully.            
+            # until the generation runs successfully.
             generator_success = True
             while True:
                 try:
@@ -155,7 +154,8 @@ def persistent_gp_mt_ax_gen_f(H, persis_info, gen_specs, libE_info):
                 device=torch.device(torch_device)
             )
 
-            # 5) Select max-utility points from the low fidelity batch to generate a high fidelity batch.
+            # 5) Select max-utility points from the low fidelity batch to
+            # generate a high fidelity batch.
             gr = max_utility_from_GP(
                 n=n_opt_hifi,
                 m=m,
@@ -211,8 +211,9 @@ def persistent_gp_mt_ax_gen_f(H, persis_info, gen_specs, libE_info):
 
 def max_utility_from_GP(n, m, gr, hifi_task):
     """
-    High fidelity batches are constructed by selecting the maximum utility points
-    from the low fidelity batch, after updating the model with the low fidelity results.
+    High fidelity batches are constructed by selecting the maximum utility
+    points from the low fidelity batch, after updating the model with the low
+    fidelity results.
     This function selects the max utility points according to the MTGP
     predictions.
     """
@@ -227,90 +228,7 @@ def max_utility_from_GP(n, m, gr, hifi_task):
     u = -np.array(f['hifi_metric'])
     best_arm_indx = np.flip(np.argsort(u))[:n]
     gr_new = GeneratorRun(
-        arms = [
-            gr.arms[i] for i in best_arm_indx
-        ],
-        weights = [1.] * n,
+        arms=[gr.arms[i] for i in best_arm_indx],
+        weights=[1.] * n,
     )
     return gr_new
-
-
-# Imports and definitions needed for custom `get_MTGP`
-from ax.core.data import Data
-from ax.core.experiment import Experiment
-from typing import Optional
-from ax.modelbridge.transforms.convert_metric_names import (
-    tconfig_from_mt_experiment
-)
-from ax.modelbridge.registry import (
-    MT_MTGP_trans,
-    ST_MTGP_trans,
-)
-from ax.modelbridge.torch import TorchModelBridge
-from ax.models.torch.botorch import BotorchModel
-DEFAULT_TORCH_DEVICE = torch.device("cpu")
-
-def get_MTGP(
-    experiment: Experiment,
-    data: Data,
-    search_space: Optional[SearchSpace] = None,
-    dtype: torch.dtype = torch.double,
-    device: torch.device = DEFAULT_TORCH_DEVICE,
-    trial_index: Optional[int] = None,
-) -> TorchModelBridge:
-    """Instantiates a Multi-task Gaussian Process (MTGP) model that generates
-    points with EI.
-    If the input experiment is a MultiTypeExperiment then a
-    Multi-type Multi-task GP model will be instantiated.
-    Otherwise, the model will be a Single-type Multi-task GP.
-
-    This method is a custom version of `get_MTGP` in `ax.modelbridge.factory`
-    that exposes a `device` parameter in order to be able to run on the GPU.
-    See https://github.com/facebook/Ax/issues/928 for details.
-
-    Implemented by S. Jalas.
-    """
-
-    if isinstance(experiment, MultiTypeExperiment):
-        trial_index_to_type = {
-            t.index: t.trial_type for t in experiment.trials.values()
-        }
-        transforms = MT_MTGP_trans
-        transform_configs = {
-            "TrialAsTask": {"trial_level_map": {"trial_type": trial_index_to_type}},
-            "ConvertMetricNames": tconfig_from_mt_experiment(experiment),
-        }
-    else:
-        # Set transforms for a Single-type MTGP model.
-        transforms = ST_MTGP_trans
-        transform_configs = None
-
-    # Choose the status quo features for the experiment from the selected trial.
-    # If trial_index is None, we will look for a status quo from the last
-    # experiment trial to use as a status quo for the experiment.
-    if trial_index is None:
-        trial_index = len(experiment.trials) - 1
-    elif trial_index >= len(experiment.trials):
-        raise ValueError("trial_index is bigger than the number of experiment trials")
-
-    status_quo = experiment.trials[trial_index].status_quo
-    if status_quo is None:
-        status_quo_features = None
-    else:
-        status_quo_features = ObservationFeatures(
-            parameters=status_quo.parameters,
-            trial_index=trial_index,
-        )
-
-    return TorchModelBridge(
-        experiment=experiment,
-        search_space=search_space or experiment.search_space,
-        data=data,
-        model=BotorchModel(),
-        transforms=transforms,
-        transform_configs=transform_configs,
-        torch_dtype=torch.double,
-        torch_device=device,
-        status_quo_features=status_quo_features,
-    )
-
