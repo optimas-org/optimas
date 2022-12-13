d4ad92e5f7f6afa5253bd11dbeaa1a82824c1b3f
Angel Ferran Pousa <angel.ferran.pousa@desy.de>
2022-12-13 16:52:18 +0100

    Update simulation function to new design

--- libe_opt/sim_functions.py
+++ libe_opt/sim_functions.py
@@ -7,7 +7,7 @@ from libensemble.executors.executor import Executor
 from libensemble.message_numbers import WORKER_DONE, TASK_FAILED
 
 
-def run_simulation(H, persis_info, sim_specs, libE_info):
+def run_template_simulation(H, persis_info, sim_specs, libE_info):
     """
     This function runs a simulation and returns quantity 'f' as well as
     other physical quantities measured in the run for convenience. Status check
@@ -16,32 +16,22 @@ def run_simulation(H, persis_info, sim_specs, libE_info):
     # By default, indicate that task failed
     calc_status = TASK_FAILED
 
+    input_values = {}
+    for name in H.dtype.names:
+        input_values[name] = H[name][0]
+
     # Modify the input script, with the value passed in H
-    values = list(H['x'][0])
-    names = sim_specs['user']['var_params']
+    # values = list(H['x'][0])
+    # names = H.dtype.names
     # Note: The order of keys is well-defined here,
     # since `varying_parameters` is an OrderedDict
 
-    # If a fidelity is present, add to list of names and values.
-    if 'z' in H.dtype.names:
-        z_name = sim_specs['user']['z_name']
-        z =  H['z'][0]
-        # If fidelity is a string, add single quotes so that it is written as
-        # a Python string by jinja.
-        if isinstance(z, str):
-            z = "'{}'".format(z)
-        values.append(z)
-        # Add fidelity name to names list only once.
-        if z_name not in names:
-            names.append(z_name)
-
-
     # Merge lists into dictionary.
-    values_dict = { n: v for n, v in zip(names, values) }
+    # values_dict = { n: v for n, v in zip(names, values) }
 
-    # If running with multiple tasks, add to names and values.
-    if 'task' in H.dtype.names:
-        values_dict['task'] = "'{}'".format(H['task'][0])
+    # # If running with multiple tasks, add to names and values.
+    # if 'task' in H.dtype.names:
+    #     values_dict['task'] = "'{}'".format(H['task'][0])
 
     # Create simulation input file.
     sim_template = sim_specs['user']['sim_template']
@@ -49,7 +39,7 @@ def run_simulation(H, persis_info, sim_specs, libE_info):
     with open(sim_template, 'r') as f:
         template = jinja2.Template( f.read() )
     with open(sim_script, 'w') as f:
-        f.write( template.render(values_dict) )
+        f.write(template.render(input_values))
     os.remove(sim_template)
 
     # Passed to command line in addition to the executable.
@@ -124,8 +114,8 @@ def run_simulation(H, persis_info, sim_specs, libE_info):
     # Prepare the array that is returned to libE
     # Automatically include the input parameters
     libE_output = np.zeros(1, dtype=sim_specs['out'])
-    for i, name in enumerate(names):
-        libE_output[name] = values[i]
+    for name in H.dtype.names:
+        libE_output[name] = H[name][0]
 
     # Data analysis from the last simulation
     if calc_status == WORKER_DONE:
