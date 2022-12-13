795cf7b5cab25b44cd3aae7bdf9b45c3d162452d
Angel Ferran Pousa <angel.ferran.pousa@desy.de>
2022-12-13 16:49:45 +0100

    Implement base generator

---
+++ libe_opt/generators/base.py
@@ -0,0 +1,65 @@
+from libe_opt.gen_functions import persistent_generator
+from libe_opt.core import Variable, Objective, Trial, Evaluation
+
+
+class Generator():
+    def __init__(self, variables, objectives=None, constraints=None, use_cuda=False):
+        if objectives is None:
+            objectives = [Objective()]
+        self.variables = variables
+        self.objectives = objectives
+        self.constraints = constraints
+        self.use_cuda = use_cuda
+        self.gen_function = persistent_generator
+        self._trials = []
+
+    def ask(self, n_trials):
+        trials = []
+        for i in range(n_trials):
+            trials.append(
+                Trial(
+                    variables=self.variables,
+                    objectives=self.objectives,
+                    index=len(self._trials) + i
+                )
+            )
+        trials = self._ask(trials)
+        self._trials.extend(trials)
+        return trials
+
+    def tell(self, trials):
+        self._tell(trials)
+
+    def get_gen_specs(self, sim_workers):
+        gen_specs = {
+            # Generator function.
+            'gen_f': self.gen_function,
+            # Generator input. This is a RNG, no need for inputs.
+            'in': ['sim_id'],
+            'persis_in': ['sim_id', 'trial_index'] + [obj.name for obj in self.objectives],
+            'out': [(var.name, float) for var in self.variables] + [
+                ('resource_sets', int),
+                ('trial_index', int),
+            ],
+            'user': {
+                'generator': self,
+                # Total max number of sims running concurrently.
+                'gen_batch_size': sim_workers,
+                # Allow generator to run on GPU.
+                'use_cuda': self.use_cuda
+            }
+        }
+        return gen_specs
+    
+    def get_libe_specs(self):
+        libE_specs = {}
+        # If not using CUDA, do not allocate resources for generator.
+        if not self.use_cuda:
+            libE_specs['zero_resource_workers'] = [1]
+        return libE_specs
+
+    def _ask(self, trials):
+        pass
+
+    def _tell(self, val, noise=None):
+        pass
