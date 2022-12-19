376b82b852366de8db13e3f07d7738d7091af75e
Angel Ferran Pousa <angel.ferran.pousa@desy.de>
2022-12-20 00:32:54 +0100

    Change logger style

--- libe_opt/utils/logger.py
+++ libe_opt/utils/logger.py
@@ -9,9 +9,10 @@ def get_logger(name, level=logging.INFO):
     # Set level.
     logger.setLevel(level)
 
-    # Set up format (level + message).
-    utils_logformat = "%(levelname)s: %(message)s"
-    formatter = logging.Formatter(utils_logformat)
+    # Set up format.
+    formatter = logging.Formatter(
+        fmt="[%(levelname)s %(asctime)s] %(name)s: %(message)s",
+        datefmt="%m-%d %H:%M:%S")
 
     # Add handler to log to standard error.
     sth = logging.StreamHandler(stream=sys.stderr)
