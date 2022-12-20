93c1a741943d68fb1a7ab68a10cba940202537c3
Ángel Ferran Pousa <angel.ferran.pousa@desy.de>
2022-12-20 10:30:40 +0100

    Remove example

--- examples/dummy_grid_search_single/varying_parameters.py
+++
@@ -1,8 +0,0 @@
-from collections import OrderedDict
-
-# List the names of the varying parameters, and their range of value
-# The names must be the same as those used in 'template_fbpic_script.py'
-varying_parameters = OrderedDict({
-    'x0': [0, 15, 5, 10],
-    'x1': [0, 15, 7, 30],
-})
