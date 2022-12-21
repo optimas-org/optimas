b7363f0a75ac899093c77a2fa764986f3c9dd644
Angel Ferran Pousa <angel.ferran.pousa@desy.de>
2022-12-21 17:35:57 +0100

    Add `default_value` argument to `VaryingParameter`
    This is useful for generators where not all variables are scanned at the
    same time, and only a default value for the others needs to be provided.

--- libe_opt/core/parameter.py
+++ libe_opt/core/parameter.py
@@ -13,12 +13,13 @@ class Parameter(NamedBase):
 
 class VaryingParameter(Parameter):
     def __init__(self, name, lower_bound, upper_bound, is_fidelity=False,
-                 fidelity_target_value=None, dtype=float):
+                 fidelity_target_value=None, default_value=None, dtype=float):
         super().__init__(name, dtype)
         self._lower_bound = lower_bound
         self._upper_bound = upper_bound
         self._is_fidelity = is_fidelity
         self._fidelity_target_value = fidelity_target_value
+        self._default_value = default_value
 
     @property
     def lower_bound(self):
@@ -36,6 +37,9 @@ class VaryingParameter(Parameter):
     def fidelity_target_value(self):
         return self._fidelity_target_value
 
+    @property
+    def default_value(self):
+        return self._default_value
 
 class TrialParameter(Parameter):
     def __init__(self, name, save_name=None, dtype=float):
