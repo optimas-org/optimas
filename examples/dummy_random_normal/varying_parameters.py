e0764592e9eb2456bac13a3533b4a438a29051fc
Angel Ferran Pousa <angel.ferran.pousa@desy.de>
2023-01-24 17:17:27 +0100

    Remove old code

--- examples/dummy_random_normal/varying_parameters.py
+++
@@ -1,8 +0,0 @@
-from collections import OrderedDict
-
-# List the names of the varying parameters, and their range of value
-# The names must be the same as those used in 'template_fbpic_script.py'
-varying_parameters = OrderedDict({
-    'x0': [7.5, 2.0],  # First element is the Normal center, second the is the sigma
-    'x1': [7.5, 2.0],
-})
