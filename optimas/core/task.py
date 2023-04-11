e12039d5647be29afc99977c0e89b8dd4ed69335
Ángel Ferran Pousa <angel.ferran.pousa@desy.de>
2023-04-11 10:58:05 -0700

    Improve `Task` docstring

--- optimas/core/task.py
+++ optimas/core/task.py
@@ -11,9 +11,9 @@ class Task(NamedBase):
     name : str
         Name of the task.
     n_init : int
-        Number of task evaluations to perform during initialization.
+        Number of task evaluations to perform in the initialization batch.
     n_opt : int
-        Number of task evaluations to perform during optimization.
+        Number of task evaluations to perform per optimization batch.
     """
     def __init__(
         self,
