078f55138b088395283801bb40ff8e004b4865cd
Angel Ferran Pousa <angel.ferran.pousa@desy.de>
2023-08-04 09:48:24 +0200

    Implement chain evaluation in sim_function

--- optimas/sim_functions.py
+++ optimas/sim_functions.py
@@ -26,36 +26,70 @@ def run_template_simulation(H, persis_info, sim_specs, libE_info):
             value = "'{}'".format(value)
         input_values[name] = value
 
+    # Prepare the array that is returned to libE
+    # Automatically include the input parameters
+    libE_output = np.zeros(1, dtype=sim_specs['out'])
+    for name in H.dtype.names:
+        libE_output[name] = H[name][0]
+
     # Get user specs.
     if 'task' in H.dtype.names:
         task_name = H['task'][0]
         user_specs = sim_specs['user'][task_name]
     else:
         user_specs = sim_specs['user']
-    sim_template = user_specs['sim_template']
-    analysis_func = user_specs['analysis_func']
-    app_name = user_specs['app_name']
 
+    # Launch and analyze the simulation(s).
+    if 'steps' in user_specs:
+        for step in user_specs['steps']:
+            step_specs = user_specs[step]
+            calc_status = execute_and_analyze_simulation(
+                app_name=step_specs['app_name'],
+                sim_template=step_specs['sim_template'],
+                input_values=input_values,
+                analysis_func=step_specs['analysis_func'],
+                libE_output=libE_output,
+                num_procs=step_specs['num_procs'],
+                num_gpus=step_specs['num_gpus']
+            )
+            # If a step has failed, do not continue with next steps.
+            if calc_status != WORKER_DONE:
+                break
+    else:
+        calc_status = execute_and_analyze_simulation(
+            app_name=user_specs['app_name'],
+            sim_template=user_specs['sim_template'],
+            input_values=input_values,
+            analysis_func=user_specs['analysis_func'],
+            libE_output=libE_output
+        )
+
+    return libE_output, persis_info, calc_status
+
+
+def execute_and_analyze_simulation(app_name, sim_template, input_values,
+                                   analysis_func, libE_output, num_procs=None,
+                                   num_gpus=None):
     # Create simulation input file.
-    sim_script = sim_template[len('template_'):]  # Strip 'template_' from name
     with open(sim_template, 'r') as f:
         template = jinja2.Template(f.read())
-    with open(sim_script, 'w') as f:
+    with open(sim_template, 'w') as f:
         f.write(template.render(input_values))
-    os.remove(sim_template)
 
     # If the template is a python file, no need to provide it as argument
     # (it has already been registered by libEnsemble as such).
-    if sim_script.endswith('.py'):
-        sim_script = None
+    if sim_template.endswith('.py'):
+        sim_template = None
 
     # Passed to command line in addition to the executable.
     exctr = Executor.executor  # Get Executor
 
     task = exctr.submit(app_name=app_name,
-                        app_args=sim_script,
+                        app_args=sim_template,
                         stdout='out.txt',
                         stderr='err.txt',
+                        num_procs=num_procs,
+                        num_gpus=num_gpus
                         )
 
     # Wait for simulation to complete
@@ -71,19 +105,13 @@ def run_template_simulation(H, persis_info, sim_specs, libE_info):
             print("Warning: Task {} in unknown state {}. Error code {}"
                   .format(task.name, task.state, task.errcode))
 
-    # Prepare the array that is returned to libE
-    # Automatically include the input parameters
-    libE_output = np.zeros(1, dtype=sim_specs['out'])
-    for name in H.dtype.names:
-        libE_output[name] = H[name][0]
-
     # Data analysis from the last simulation
     if calc_status == WORKER_DONE:
         # Extract the objective function for the current simulation,
         # as well as a few diagnostics
         analysis_func(task.workdir, libE_output)
 
-    return libE_output, persis_info, calc_status
+    return calc_status
 
 
 def run_function(H, persis_info, sim_specs, libE_info):
