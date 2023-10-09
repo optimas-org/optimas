08a4070d39d096def7a4e54d6f6a09ff8d086e41
Ángel Ferran Pousa <angel.ferran.pousa@desy.de>
2023-10-09 19:32:49 +0200

    Update test

--- tests/test_evaluation_diagnostics.py
+++ tests/test_evaluation_diagnostics.py
@@ -61,18 +61,39 @@ def test_evaluation_diagnostics():
             exploration.history[name]
         )
 
-    for p in gen.varying_parameters:
-        assert p.json() == diags.varying_parameters[p.name].json()
-    for p in gen.objectives:
-        assert p.json() == diags.objectives[p.name].json()
-    for p in gen.analyzed_parameters:
-        assert p.json() == diags.analyzed_parameters[p.name].json()
+    for p_in, p_out in zip(gen.varying_parameters, diags.varying_parameters):
+        assert p_in.json() == p_out.json()
+    for p_in, p_out in zip(gen.objectives, diags.objectives):
+        assert p_in.json() == p_out.json()
+    for p_in, p_out in zip(gen.analyzed_parameters, diags.analyzed_parameters):
+        assert p_in.json() == p_out.json()
 
     diags.plot_objective(show_trace=True)
     plt.savefig(os.path.join(exploration_dir_path, 'optimization.png'))
 
     diags.plot_worker_timeline()
     plt.savefig(os.path.join(exploration_dir_path, 'timeline.png'))
+
+    # Check that all 3 possible objective inputs give the same result.
+    _, trace1 = diags.get_objective_trace()
+    _, trace2 = diags.get_objective_trace('f1')
+    _, trace3 = diags.get_objective_trace(obj)
+    np.testing.assert_array_equal(trace1, trace2)
+    np.testing.assert_array_equal(trace1, trace3)
+
+    # Test making plot using the diagnostics API.
+    fig, ax = plt.subplots()
+    vps = diags.varying_parameters
+    df = diags.df
+    f1 = diags.objectives[0]
+    ax.axvline(vps[0].lower_bound)
+    ax.axvline(vps[0].upper_bound)
+    ax.set_xlabel(vps[0].name)
+    ax.axhline(vps[1].lower_bound)
+    ax.axhline(vps[1].upper_bound)
+    ax.set_ylabel(vps[1].name)
+    ax.scatter(df[vps[0].name], df[vps[1].name], c=df[f1.name])
+    fig.savefig(os.path.join(exploration_dir_path, 'search_space.png'))
 
 
 if __name__ == '__main__':
