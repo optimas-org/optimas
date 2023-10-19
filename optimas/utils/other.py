cf612111557e25c0fd7e28f611d40fc5ce9f3e18
Angel Ferran Pousa <angel.ferran.pousa@desy.de>
2023-10-19 18:33:42 +0200

    Implement `convert_to_dataframe`

--- optimas/utils/other.py
+++ optimas/utils/other.py
@@ -1,5 +1,8 @@
 """Definition of other utilities used internally by optimas."""
-from typing import Any
+from typing import Any, Union, List, Dict
+
+import numpy as np
+import pandas as pd
 
 
 def update_object(object_old: Any, object_new: Any) -> None:
@@ -19,3 +22,38 @@ def update_object(object_old: Any, object_new: Any) -> None:
     assert isinstance(object_new, type(object_old)), "Object types don't match"
     for key, value in vars(object_new).items():
         setattr(object_old, key, value)
+
+
+def convert_to_dataframe(
+    data: Union[Dict, List[Dict], np.ndarray, pd.DataFrame]
+) -> pd.DataFrame:
+    """Convert input data to a pandas DataFrame.
+
+    Parameters
+    ----------
+    data : dict of lists, list of dicts, ndarray or DataFrame
+        The input data, which can be a dictionary of lists, a list of
+        dictionaries, a numpy structured array or a pandas dataframe.
+
+    Returns
+    -------
+    pd.DataFrame
+        The converted input data.
+
+    Raises
+    ------
+    ValueError
+        If the type of the input data is not supported.
+    """
+    # Get fields in given data.
+    if isinstance(data, np.ndarray):
+        d = {label: data[label].flatten() for label in data.dtype.names}
+        return pd.DataFrame(d)
+    elif isinstance(data, pd.DataFrame):
+        return data
+    elif isinstance(data, dict) or isinstance(data, list):
+        return pd.DataFrame(data)
+    else:
+        raise ValueError(
+            f"Cannot convert {type(data)} to a pandas dataframe."
+        )
