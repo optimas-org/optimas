4ae1bea312bcf9aaeee05ea2b091aac4c9baa885
Remi Lehe <remi.lehe@normalesup.org>
2023-12-15 09:02:14 -0800

    Make Ax optional for optimas

--- optimas/generators/__init__.py
+++ optimas/generators/__init__.py
@@ -1,7 +1,21 @@
-from .ax.service.single_fidelity import AxSingleFidelityGenerator
-from .ax.service.multi_fidelity import AxMultiFidelityGenerator
-from .ax.service.ax_client import AxClientGenerator
-from .ax.developer.multitask import AxMultitaskGenerator
+# Import Ax generators
+try:
+    from .ax.service.single_fidelity import AxSingleFidelityGenerator
+    from .ax.service.multi_fidelity import AxMultiFidelityGenerator
+    from .ax.service.ax_client import AxClientGenerator
+    from .ax.developer.multitask import AxMultitaskGenerator
+except ImportError as e:
+    if e.__str__() == "No module named 'ax'":
+        # Replace generators by dummy generators that will
+        # raise an error only if the user tries to instantiate them
+        # and tell them to install ax-platform
+        from .ax.import_error_dummy_generator import AxImportErrorDummyGenerator
+        AxSingleFidelityGenerator = AxImportErrorDummyGenerator
+        AxMultiFidelityGenerator = AxImportErrorDummyGenerator
+        AxClientGenerator = AxImportErrorDummyGenerator
+        AxMultitaskGenerator = AxImportErrorDummyGenerator
+
+# Import optimas native generators
 from .grid_sampling import GridSamplingGenerator
 from .line_sampling import LineSamplingGenerator
 from .random_sampling import RandomSamplingGenerator
