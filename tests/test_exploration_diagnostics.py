6fc97a2b6b8f21d908c3eba636819d98151192eb
Ángel Ferran Pousa <angel.ferran.pousa@desy.de>
2023-12-21 15:38:00 +0100

    Update test

--- tests/test_exploration_diagnostics.py
+++ tests/test_exploration_diagnostics.py
@@ -48,50 +48,48 @@ def test_exploration_diagnostics():
     # Run exploration.
     exploration.run()
 
-    # Test diagnostics
-    diags = ExplorationDiagnostics(
-        exploration_dir_path,
-        relative_start_time=False,
-        remove_unfinished_evaluations=False,
-    )
-    for name in exploration.history:
-        np.testing.assert_array_equal(
-            diags.history[name].to_numpy(), exploration.history[name].to_numpy()
-        )
+    # Test diagnostics with both possible initializations.
+    diags_from_file = ExplorationDiagnostics(exploration_dir_path)
+    diags_from_instance = ExplorationDiagnostics(exploration)
+    for diags in [diags_from_file, diags_from_instance]:
+        for name in exploration.history:
+            np.testing.assert_array_equal(
+                diags.history[name], exploration.history[name]
+            )
 
-    for p_in, p_out in zip(gen.varying_parameters, diags.varying_parameters):
-        assert p_in.model_dump_json() == p_out.model_dump_json()
-    for p_in, p_out in zip(gen.objectives, diags.objectives):
-        assert p_in.model_dump_json() == p_out.model_dump_json()
-    for p_in, p_out in zip(gen.analyzed_parameters, diags.analyzed_parameters):
-        assert p_in.model_dump_json() == p_out.model_dump_json()
+        for p_i, p_o in zip(gen.varying_parameters, diags.varying_parameters):
+            assert p_i.model_dump_json() == p_o.model_dump_json()
+        for p_i, p_o in zip(gen.objectives, diags.objectives):
+            assert p_i.model_dump_json() == p_o.model_dump_json()
+        for p_i, p_o in zip(gen.analyzed_parameters, diags.analyzed_parameters):
+            assert p_i.model_dump_json() == p_o.model_dump_json()
 
-    diags.plot_objective(show_trace=True)
-    plt.savefig(os.path.join(exploration_dir_path, "optimization.png"))
+        diags.plot_objective(show_trace=True)
+        plt.savefig(os.path.join(exploration_dir_path, "optimization.png"))
 
-    diags.plot_worker_timeline()
-    plt.savefig(os.path.join(exploration_dir_path, "timeline.png"))
+        diags.plot_worker_timeline()
+        plt.savefig(os.path.join(exploration_dir_path, "timeline.png"))
 
-    # Check that all 3 possible objective inputs give the same result.
-    _, trace1 = diags.get_objective_trace()
-    _, trace2 = diags.get_objective_trace("f1")
-    _, trace3 = diags.get_objective_trace(obj)
-    np.testing.assert_array_equal(trace1, trace2)
-    np.testing.assert_array_equal(trace1, trace3)
+        # Check that all 3 possible objective inputs give the same result.
+        _, trace1 = diags.get_objective_trace()
+        _, trace2 = diags.get_objective_trace("f1")
+        _, trace3 = diags.get_objective_trace(obj)
+        np.testing.assert_array_equal(trace1, trace2)
+        np.testing.assert_array_equal(trace1, trace3)
 
-    # Test making plot using the diagnostics API.
-    fig, ax = plt.subplots()
-    vps = diags.varying_parameters
-    df = diags.history
-    f1 = diags.objectives[0]
-    ax.axvline(vps[0].lower_bound)
-    ax.axvline(vps[0].upper_bound)
-    ax.set_xlabel(vps[0].name)
-    ax.axhline(vps[1].lower_bound)
-    ax.axhline(vps[1].upper_bound)
-    ax.set_ylabel(vps[1].name)
-    ax.scatter(df[vps[0].name], df[vps[1].name], c=df[f1.name])
-    fig.savefig(os.path.join(exploration_dir_path, "search_space.png"))
+        # Test making plot using the diagnostics API.
+        fig, ax = plt.subplots()
+        vps = diags.varying_parameters
+        df = diags.history
+        f1 = diags.objectives[0]
+        ax.axvline(vps[0].lower_bound)
+        ax.axvline(vps[0].upper_bound)
+        ax.set_xlabel(vps[0].name)
+        ax.axhline(vps[1].lower_bound)
+        ax.axhline(vps[1].upper_bound)
+        ax.set_ylabel(vps[1].name)
+        ax.scatter(df[vps[0].name], df[vps[1].name], c=df[f1.name])
+        fig.savefig(os.path.join(exploration_dir_path, "search_space.png"))
 
 
 if __name__ == "__main__":
