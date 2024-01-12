ec013dce83cc950d3a2591c04f79d85ac711fb68
Ángel Ferran Pousa <angel.ferran.pousa@desy.de>
2024-01-12 10:28:47 +0100

    Add sleep option to test template script

--- tests/resources/template_simulation_script.py
+++ tests/resources/template_simulation_script.py
@@ -4,9 +4,14 @@ The template takes two parameters x0 and x1 as input
 and stores the result in `result.txt`.
 """
 import os
+import time
 import numpy as np
 
 test_env_var = os.getenv("LIBE_TEST_SUB_ENV_VAR")
+sleep = os.getenv("OPTIMAS_TEST_SLEEP")
+
+if sleep is not None:
+    time.sleep(float(sleep))
 
 # 2D function with multiple minima
 result = -({{x0}} + 10 * np.cos({{x0}})) * ({{x1}} + 5 * np.cos({{x1}}))
