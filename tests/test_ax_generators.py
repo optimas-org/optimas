ac20ce56ca8b03d21225f61c5e2fd3744edd5d85
Ángel Ferran Pousa <angel.ferran.pousa@desy.de>
2024-02-15 16:47:15 +0100

    Fix test

--- tests/test_ax_generators.py
+++ tests/test_ax_generators.py
@@ -588,7 +588,7 @@ def test_ax_client():
     exploration = Exploration(
         generator=gen,
         evaluator=ev,
-        max_evals=13,
+        max_evals=15,
         sim_workers=2,
         run_async=False,
         exploration_dir_path="./tests_output/test_ax_client",
