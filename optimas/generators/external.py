298772ce4b2a75f970693dccbd9297fc872ebfd4
shudson <shudson@anl.gov>
2024-11-08 10:17:27 -0600

    Add required docstrings

--- optimas/generators/external.py
+++ optimas/generators/external.py
@@ -1,8 +1,10 @@
+"""Contains the definition of an external generator."""
+
 from .base import Generator
 
 
 class ExternalGenerator(Generator):
-    """Supports a generator in the CAMPA generator standard"""
+    """Supports a generator in the CAMPA generator standard."""
 
     def __init__(
         self,
@@ -15,7 +17,9 @@ class ExternalGenerator(Generator):
         self.gen = ext_gen
 
     def ask(self, n_trials):
+        """Request the next set of points to evaluate."""
         return self.gen.ask(n_trials)
 
     def tell(self, trials):
+        """Send the results of evaluations to the generator."""
         self.gen.tell(trials)
