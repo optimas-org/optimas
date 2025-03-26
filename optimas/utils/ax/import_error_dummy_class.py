0e24039064e7d49bb64210fabbcfa1f31a4369c0
Remi Lehe <remi.lehe@normalesup.org>
2025-03-26 11:54:17 -0700

    Ensure optimas can be run without ax

---
+++ optimas/utils/ax/import_error_dummy_class.py
@@ -0,0 +1,15 @@
+"""Contains the definition of dummy class that raises an import error."""
+
+
+class AxImportErrorDummyClass(object):
+    """Class that raises an error when instantiated, telling the user to install ax-platform.
+
+    This class replaces all other Ax-based classes, when Ax is not installed
+    """
+
+    def __init__(self, *args, **kwargs) -> None:
+        raise RuntimeError(
+            "You need to install ax-platform, in order "
+            "to use Ax-based generators in optimas.\n"
+            "e.g. with `pip install ax-platform > 0.5.0`"
+        )
